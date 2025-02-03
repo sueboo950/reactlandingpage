d6ae1138e9d93780ebe3cb162200a7a1140a7ab6
sophie950 <sueboo950950@gmail.com>
2025-02-03 17:39:53 -0600

    fixed the file path of the index.html file in the webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'index.html',
+            template: './index.html',
             filename: 'index.html'
         })
     ]
